7b83914f6d80637adc763d860bae2c20e54fa8c3
YoussefBeshnack <youssefbeshnack@gmail.com>
2025-12-21 02:35:23 +0200

    api url

--- src/js/Modules/api.js
+++ src/js/Modules/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://web-design-project-three.vercel.app/:8000/api";
+const BASE_URL = "https://web-design-project-three.vercel.app/api";
 
 export async function apiFetch(
   endpoint,
